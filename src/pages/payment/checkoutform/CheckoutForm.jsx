7977eff5b01c8e58f225ffe7a48ce72051f95a2c
wakai-megumi <tarun483sharma@gmail.com>
2023-06-21 10:28:11 +0530

    production

--- src/pages/payment/checkoutform/CheckoutForm.jsx
+++ src/pages/payment/checkoutform/CheckoutForm.jsx
@@ -70,6 +70,7 @@ const CheckoutForm = () => {
             confirmParams: {
                 // Make sure to change this to your payment completion page
                 return_url: `https://waneem.onrender.com/payment_success`,
+
             },
         });
 
